42b6dc9665797c625b68737bb11399db8d8af79e
이규빈 <skdba1313@gmail.com>
2026-02-08 13:51:42 +0900

    수정 : 상품 빈화면 출력 개선

--- src/pages/product/Product.vue
+++ src/pages/product/Product.vue
@@ -424,11 +424,9 @@ onUnmounted(() => {
 
 @keyframes slideUp {
   from {
-    opacity: 0;
     transform: translateY(10px);
   }
   to {
-    opacity: 1;
     transform: translateY(0);
   }
 }
--- src/pages/product/ProductHome.vue
+++ src/pages/product/ProductHome.vue
@@ -181,11 +181,9 @@ onMounted(async () => {
 
 @keyframes slideUp {
   from {
-    opacity: 0;
     transform: translateY(10px);
   }
   to {
-    opacity: 1;
     transform: translateY(0);
   }
 }
